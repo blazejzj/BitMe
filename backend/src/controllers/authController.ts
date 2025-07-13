1c5f1c7feebe24310a94517f506aa50096292ed3
blazejzj <opexrrator@gmail.com>
2025-07-13 20:04:24 +0200

    feat: added logic for loggin in, registering and logging out, have to add validation

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -15,3 +15,53 @@ exports.registerUser = async (req: any, res: any) => {
 
     res.status(200).json({ message: "Successfully registered." });
 };
+
+exports.login = async (req: any, res: any) => {
+    // TODO
+    // Validation of input fields has to be done
+
+    const { email, password } = req.body;
+    const user = await db.getUserByEmail(email);
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!user || !passwordMatch)
+        return res.status(401).json({ message: "Wrong username or password." });
+
+    const payload = {
+        id: user.id,
+        email: user.email,
+        photoUrl: user.photoUrl,
+        role: user.role,
+        creationDate: user.creationDate,
+        lastSeenAt: user.lastSeenAt,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        exp: "1h",
+    });
+
+    // set cookie with the token we generated here
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: false, // this aint good xd but for localhost ok
+        maxAge: 3600000,
+        sameSite: "strict",
+    });
+
+    res.status().json({
+        message: "Login successfull",
+    });
+};
+
+exports.logOut = (req: any, res: any) => {
+    // when we log out we clear the cookie
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: false,
+        sameSite: "strict",
+    });
+
+    res.status(200).json({
+        message: "Successfully logged out.",
+    });
+};
