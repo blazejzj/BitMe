75ac68cb31287bb6a938c1e437af476efa24ee60
blazejzj <opexrrator@gmail.com>
2025-08-16 13:10:13 +0200

    feat: added sending friend requests feature, and fetching all sent

--- backend/src/controllers/friendsController.ts
+++ backend/src/controllers/friendsController.ts
@@ -47,7 +47,52 @@ exports.getBlockedUsers = async (req: any, res: any) => {
     });
 };
 
-exports.sendFriendRequest = async (req: any, res: any) => {};
+exports.sendFriendRequest = async (req: any, res: any) => {
+    const user = req.user;
+    const friendUserId = req.params.id;
+
+    if (user.id === friendUserId) {
+        return res.status(400).json({ msg: "You can't add yourself." });
+    }
+
+    const friendAlready = await db.userConnections.isFriend(
+        user.id,
+        friendUserId
+    );
+    if (friendAlready) {
+        return res
+            .status(400)
+            .json({ msg: "User is already on your friendslist." });
+    }
+
+    const friendRequestActive = await db.userConnections.isRequestActive(
+        user.id,
+        friendUserId
+    );
+    if (friendRequestActive) {
+        return res
+            .status(400)
+            .json({ msg: "Friend request has already been sent." });
+    }
+
+    const friendUserExists = await db.user.userIdExists(friendUserId);
+    if (!friendUserExists) {
+        return res
+            .status(400)
+            .json({ msg: "Person you are trying to add does not exist." });
+    }
+
+    await db.sendFriendRequest(user.id, friendUserId);
+};
+
+exports.getAllFriendRequests = async (req: any, res: any) => {
+    const userId = req.user.id;
+    const requests = db.userConnections.getAllActiveFriendRequests(userId);
+    res.status(200).json({
+        msg: "Successfully fetched all friends requests.",
+        requests: requests,
+    });
+};
 
 exports.removeFriendRequest = async (req: any, res: any) => {};
 
--- backend/src/db/userConnections.ts
+++ backend/src/db/userConnections.ts
@@ -39,9 +39,59 @@ async function getBlockedUsers(userId: string) {
     return users;
 }
 
+async function isFriend(userId: string, friendId: string) {
+    const foundEntry = await prisma.friends.findUnique({
+        where: { userId_friendId: { userId, friendId } },
+    });
+    return !!foundEntry;
+}
+
+async function isRequestActive(userId: string, friendId: string) {
+    const found = await prisma.friendRequest.findUnique({
+        where: {
+            requestFromId_requestToId: {
+                requestFromId: userId,
+                requestToId: friendId,
+            },
+        },
+    });
+
+    return !!found;
+}
+
+async function sendFriendRequest(userId: string, friendId: string) {
+    await prisma.friendRequest.create({
+        data: {
+            requestFromId: userId,
+            requestToId: friendId,
+        },
+    });
+}
+
+async function getAllActiveFriendRequests(userId: string) {
+    const requests = await prisma.friendRequest.findMany({
+        where: {
+            requestFromId: userId,
+        },
+        include: {
+            requestTo: {
+                select: {
+                    id: true,
+                    displayName: true,
+                    username: true,
+                },
+            },
+        },
+    });
+}
+
 export default {
     isBlocked,
     block,
     unblock,
     getBlockedUsers,
+    isFriend,
+    isRequestActive,
+    sendFriendRequest,
+    getAllActiveFriendRequests,
 };
